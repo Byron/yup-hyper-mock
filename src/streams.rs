a0b97946f1db382736a16dd2c01fa8da52800d77
Erich Gubler <erichdongubler@gmail.com>
2020-01-10 12:12:48 -0700

    feat: update to `async`/`await`

--- src/streams.rs
+++ src/streams.rs
@@ -1,135 +1,66 @@
 use std::str;
-use std::io::{self, Read, Write};
+use std::io;
+use std::{cmp::min, pin::Pin, task::{Context, Poll, Waker}};
 
-use futures;
-use futures::task;
-use tokio_io::{AsyncRead, AsyncWrite};
-use tokio_reactor::PollEvented;
-use mio::{event::Evented, PollOpt, Poll, Ready, Token, SetReadiness, Registration};
+use hyper::client::connect::{Connected, Connection};
+use tokio::io::{AsyncRead, AsyncWrite};
 
-struct MockHttpStream {
+pub struct MockPollStream {
     data: Vec<u8>,
     pos: usize,
     ready_for_response: bool,
-    task: Option<task::Task>,
-    registration: Registration,
-}
-
-pub struct MockPollStream {
-    stream: PollEvented<MockHttpStream>,
-    set_readiness: SetReadiness
+    waker: Option<Waker>
 }
 
 impl MockPollStream {
-    pub fn new(data: Vec<u8>) -> MockPollStream {
-        let (registration, set_readiness) = Registration::new2();
-        let m = MockHttpStream {
-            data: data,
+    pub fn new(data: Vec<u8>) -> Self {
+        Self {
+            data,
             pos: 0,
             ready_for_response: false,
-            task: None,
-            registration: registration
-        };
-        set_readiness.set_readiness(Ready::writable()).unwrap();
-        MockPollStream {
-            stream: PollEvented::new(m),
-            set_readiness: set_readiness
+            waker: None,
         }
     }
 }
 
-impl Evented for MockHttpStream {
-    fn register(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt)
-        -> io::Result<()>
-    {
-        Ok(self.registration.register(poll, token, interest, opts)?)
-    }
-
-    fn reregister(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt)
-        -> io::Result<()>
-    {
-        Ok(self.registration.reregister(poll, token, interest, opts)?)
-    }
-
-    fn deregister(&self, _poll: &Poll) -> io::Result<()> {
-        Ok(())
-    }
-}
-
-impl Read for MockHttpStream {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+impl AsyncRead for MockPollStream {
+    fn poll_read(mut self: Pin<&mut Self>, cx: &mut Context, buf: &mut [u8]) -> Poll<io::Result<usize>> {
         if !self.ready_for_response {
             trace!("Not ready for read yet");
-            self.task = Some(task::current());
-            return Err(io::ErrorKind::WouldBlock.into());
+            self.waker = Some(cx.waker().clone());
+            return Poll::Pending;
         }
         trace!("Buffer size: {}, Data size: {}, Pos: {}", buf.len(), self.data.len(), self.pos);
-        let n = if buf.len() < (self.data.len()-self.pos) { buf.len() } else { self.data.len()-self.pos };
-        if n > 0 {
-            for i in 0..n {
-                buf[i] = self.data[self.pos];
-                self.pos+=1;
-            }
-        }
-        trace!("Read {} bytes: '{}'", n, str::from_utf8(&buf[..n]).unwrap_or("<bad utf-8>"));
-        self.task = Some(task::current());
-        Ok(n)
-    }
-}
-
-impl AsyncRead for MockHttpStream {}
-
-impl Read for MockPollStream {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        self.stream.read(buf)
-    }
-}
-
-impl AsyncRead for MockPollStream {}
-
-impl Write for MockHttpStream {
-    fn write(&mut self, data: &[u8]) -> io::Result<usize> {
-        trace!("Request data: {}", str::from_utf8(data).unwrap_or("<bad utf-8>"));
-        self.ready_for_response = true;
-        Ok(data.len())
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        Ok(())
-    }
-}
-
-impl AsyncWrite for MockHttpStream {
-    fn shutdown(&mut self) -> futures::Poll<(), io::Error> {
-        Ok(().into())
-    }
-}
-
-impl Write for MockPollStream {
-    fn write(&mut self, data: &[u8]) -> io::Result<usize> {
-        let ret = self.stream.write(data);
-        match ret {
-            Ok(r) => {
-                self.set_readiness.set_readiness(Ready::readable()).unwrap();
-                Ok(r)
-            }
-
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                self.stream.clear_write_ready()?;
-                Err(io::Error::from(io::ErrorKind::WouldBlock))
-            }
-
-            Err(e) => Err(e),
-        }
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        self.stream.flush()
+        let n = min(buf.len(), self.data.len() - self.pos);
+        let read_until = self.pos + n;
+        buf[..n].copy_from_slice(&self.data[self.pos..read_until]);
+        self.pos = read_until;
+        trace!("Read {} bytes: '{}'", n, str::from_utf8(&buf[..n]).unwrap_or ("<bad utf-8>"));
+        self.waker = Some(cx.waker().clone());
+        Poll::Ready(Ok(n))
     }
 }
 
 impl AsyncWrite for MockPollStream {
-    fn shutdown(&mut self) -> futures::Poll<(), io::Error> {
-        self.stream.shutdown()
+    fn poll_write(self: Pin<&mut Self>, _cx: &mut Context, data: &[u8]) -> Poll<io::Result<usize>> {
+        trace!("Request data: {}", str::from_utf8(data).unwrap_or("<bad utf-8>"));
+        let Self {ready_for_response, waker, .. } = self.get_mut();
+        *ready_for_response = true;
+        waker.take().map(|w| w.wake());
+        Poll::Ready(Ok(data.len()))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<io::Result<()>> {
+        Poll::Ready(Ok(()))
+    }
+}
+
+impl Connection for MockPollStream {
+    fn connected(&self) -> Connected {
+        Connected::new()
     }
 }
